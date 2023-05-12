31bb6a18cbe08f0e67ccb721b1a08230129c3d67
Parkybin <104494169+Parkybin@users.noreply.github.com>
2023-05-12 10:28:29 +0900

    Create header.js
    header need commit

---
+++ header.js
@@ -0,0 +1,71 @@
+import React from "react";
+import './css/Header.css';
+import {BrowserRouter,Route,Routes, Link} from 'react-router-dom';
+import Map from './Map';
+import Gen_total from './Gen_total';
+import Gen_detail from './Gen_detail';
+import Gen_year from './Gen_year';
+
+class Header extends React.Component {
+render() { 
+    return(
+        <BrowserRouter>
+        <div className="headerContainer">
+                <div className="nav">
+                    <div className="item">
+                        <Link to="/">에코로케이션</Link>
+                    </div>
+                    <div className="item">
+                        <Link to="/Gen_total">과거 재생에너지 발전량</Link>
+                        <div className="dropdown">
+                            <Link to="/Gen_total">지역별 발전량</Link>
+                            <Link to="/Gen_year">연도별 발전량</Link>
+                            <Link to="/Gen_detail">지역별 발전량 상세</Link>
+                        </div>
+                    </div>
+                        
+                    <div className="item">
+                        <Link to="/">재생에너지 잠재량 확인</Link>
+                        <div className="dropdown">
+                            <Link to="/">지역별 잠재량</Link>
+                            <Link to="/">발전원별 잠재량</Link>
+                            <Link to="/">시간별 잠재량</Link>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <Link to="/">기존 발전소</Link>
+                        <div className="dropdown">
+                            <Link to="/">지역별 발전소 확인</Link>
+                            <Link to="/">발전원별 발전소 위치</Link>
+                            <Link to="/">발전소 상세정보</Link>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <Link to="/">재생에너지 전환율</Link>
+                        <div className="dropdown">
+                            <Link to="/">국내 재생에너지 생산비율</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Routes>
+                {/* 과거 재생에너지 발전량 */}
+                <Route path="/" element={<Map />}></Route>
+                <Route path="/Gen_total" element={<Gen_total />}></Route>
+                <Route path="/Gen_year" element ={<Gen_year />}></Route>
+                <Route path="/Gen_detail" element ={<Gen_detail />}></Route>
+                {/* 재생에너지 잠재력 확인 */}
+
+                {/* 기존 발전소 */}
+
+                {/* 재생에너지 전환율 */}
+
+
+            </Routes>
+        </BrowserRouter>
+        )
+    }
+}
+
+export default Header;
